d77824b9af221ad75637928dd23cf5a7d6d64c35
Ryan Camp <141803241+TheSimicSlaw@users.noreply.github.com>
2024-10-01 15:45:57 -0500

    Movement controls edit
    Rotate in place
    Car or tank controls? (Comment out car controls lines for tank controls - will decide later)

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -6,41 +6,48 @@ using UnityEngine;
 public class GameInput : MonoBehaviour
 {
 
-    private PlayerInputActions playerInputActions;
+  private PlayerInputActions playerInputActions;
 
-    private void Awake()
+  private void Awake()
+  {
+    playerInputActions = new PlayerInputActions();
+    playerInputActions.Player.Enable();
+
+  }
+
+  public bool getJumpInput()
+  {
+    if (playerInputActions.Player.Jump.ReadValue<float>() == 1)
     {
-        playerInputActions = new PlayerInputActions();
-        playerInputActions.Player.Enable();
-
+      return true;
     }
+    return false;
+  }
 
-    public bool getJumpInput()
+  public bool getSprintInput()
+  {
+    if (playerInputActions.Player.Sprint.ReadValue<float>() == 1)
+      return true;
+    return false;
+  }
+
+
+  public Vector2 getMovementInputVectorNormalized()
+  {
+    Vector2 movementInput = playerInputActions.Player.Move.ReadValue<Vector2>();
+    /*
+     if(movementInput.y == 0)
+     {
+         movementInput.x = 0;
+     }*/
+
+    /*if (movementInput.y < 0)
     {
-        if(playerInputActions.Player.Jump.ReadValue<float>() == 1)
-        {
-            return true;
-        }
-        return false;
-    }
+        movementInput.x *= -1;
+    }*/
 
+    movementInput = movementInput.normalized;
 
-    public Vector2 getMovementInputVectorNormalized()
-    {
-        Vector2 movementInput = playerInputActions.Player.Move.ReadValue<Vector2>();
-       /*
-        if(movementInput.y == 0)
-        {
-            movementInput.x = 0;
-        }*/
- 
-        if (movementInput.y < 0)
-        {
-            movementInput.x *= -1;
-        }
-
-        movementInput = movementInput.normalized;
-
-        return movementInput;
-    }
+    return movementInput;
+  }
 }
