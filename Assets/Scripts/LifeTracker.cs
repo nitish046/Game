229c260f627243d69c22aafb73283b261fe9e652
Ryan Camp <141803241+TheSimicSlaw@users.noreply.github.com>
2024-10-12 22:57:18 -0500

    Task 5a completed
    Lives system enabled
    Pressing Backspace calls LoseLife method

---
+++ Assets/Scripts/LifeTracker.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeTracker : MonoBehaviour
+{
+  public int lives;
+
+  [SerializeField]
+  public UnityEngine.UI.Image[] hearts;
+  public Sprite heartImage;
+
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Backspace))
+    {
+      LoseLife();
+    }
+
+    for (int i = 0; i <= hearts.Length; i++)
+    {
+      if (i < lives)
+      {
+        hearts[i].enabled = true;
+      }
+      else
+      {
+        hearts[i].enabled = false;
+      }
+    }
+  }
+
+  void LoseLife()
+  {
+    if (lives >= 0)
+    {
+      lives--;
+      if (lives == 0)
+      {
+        LoseGame();
+      }
+    }
+  }
+
+  void LoseGame()
+  {
+
+  }
+
+}
