98b0748f83da7c7ea5915e34c86fcedf54fa7a27
nitish046 <nitishsai20@gmail.com>
2024-10-13 18:44:20 -0500

    throwobj

---
+++ Assets/Scripts/ThrowObject.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class ProjectileLauncher : MonoBehaviour
+{
+    public Transform launchPoint;
+    public GameObject projectile;
+    public float launchSpeed = 10f;
+    public AudioSource audioSource;
+  
+    GameObject _projectile;
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _projectile = Instantiate(projectile, launchPoint.position, launchPoint.rotation);
+
+
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            audioSource.Play();
+            _projectile.GetComponent<Rigidbody>().isKinematic = false;
+            _projectile.GetComponent<Rigidbody>().velocity = launchSpeed * launchPoint.up;
+        }
+       
+    }
+
+   
+}
