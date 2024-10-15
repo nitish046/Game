cdf9acf1de76b6d8a50fff27aa361141ad7d42f6
caleb <lazerarms@gmail.com>
2024-10-15 09:10:53 -0500

    implemented some of the animations for henry and the racoon
    animations implemented
    -Raccoon: idle, walk, sprint, jump
    -henry: idle, walk

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,6 +21,7 @@ public class Player : MonoBehaviour
 
   public AudioClip laugh;
   private AudioSource audioSource;
+  Animator player_animator;
 
   private void Awake()
   {
@@ -28,6 +29,7 @@ public class Player : MonoBehaviour
     audioSource = GetComponent<AudioSource>();
     
     controller = GetComponent<CharacterController>();
+    player_animator = transform.GetChild(0).GetComponent<Animator>();
   }
 
   private void FixedUpdate()
@@ -96,11 +98,12 @@ public class Player : MonoBehaviour
         racconColliding = false;
       }
     }
-    
+
+    PlayMovementAnimation(movementInput);
     controller.Move(movementDirection * moveDistance);
     transform.rotation *= Quaternion.Euler(0, movementInput.x * rotationSpeed * Time.fixedDeltaTime, 0);
     
-    Debug.Log(groundcheck());
+    //Debug.Log(groundcheck());
     
     /*Debug.Log(racconColliding);
 
@@ -151,11 +154,13 @@ public class Player : MonoBehaviour
       //Vector3 snappedPosition = new Vector3(transform.position.x, closestPoint.y + offset, transform.position.z);
 
       //transform.position = snappedPosition;
+      PlayJumpingAnimation(false);
     }
 
     if (jump && grounded)
     {
       velocity = Mathf.Sqrt(jumpHeight * -3f * (Physics.gravity.y * gravityScale));
+      PlayJumpingAnimation(true);
     }
     controller.Move(new Vector3(0, velocity, 0) * Time.deltaTime);
 
@@ -186,4 +191,65 @@ public class Player : MonoBehaviour
 
   }
 
+
+    public void PlayMovementAnimation(Vector2 movementInput)
+    {
+        bool isWalking = player_animator.GetBool("isWalking");
+        bool isSprinting = player_animator.GetBool("isSprinting");
+        bool isJumping = player_animator.GetBool("isJumping");
+
+        if (movementInput != Vector2.zero)
+        {
+            player_animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            player_animator.SetBool("isWalking", false);
+        }
+        if(isWalking && gameInput.getSprintInput())
+        {
+            player_animator.SetBool("isSprinting", true);
+        }
+        else if(isWalking && !gameInput.getSprintInput())
+        {
+            player_animator.SetBool("isSprinting", false);
+        }
+    }
+
+    public void PlayJumpingAnimation(bool started_jumping)
+    {
+        bool isWalking = player_animator.GetBool("isWalking");
+        bool isSprinting = player_animator.GetBool("isSprinting");
+        bool isJumping = player_animator.GetBool("isJumping");
+
+        if(!isWalking && started_jumping)
+        {
+            player_animator.SetBool("isJumping", true);
+            player_animator.SetInteger("previousState", 0);
+        }
+        else if(!isWalking && !started_jumping)
+        {
+            player_animator.SetBool("isJumping", false);
+        }
+        if (!isSprinting && started_jumping)
+        {
+            player_animator.SetBool("isJumping", true);
+            player_animator.SetInteger("previousState", 1);
+        }
+        else if (!isSprinting && !started_jumping)
+        {
+            player_animator.SetBool("isJumping", false);
+        }
+        if (isSprinting && started_jumping)
+        {
+            player_animator.SetBool("isJumping", true);
+            player_animator.SetInteger("previousState", 2);
+        }
+        else if (isSprinting && !started_jumping)
+        {
+            player_animator.SetBool("isJumping", false);
+        }
+
+
+    }
 }
