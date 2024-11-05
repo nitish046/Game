b681fff8764c08120864da0a15ab021dd0b30707
Ryan Camp <141803241+TheSimicSlaw@users.noreply.github.com>
2024-11-04 22:18:54 -0600

    Fixed lifetracker so it doesn't cause you to win if you run out of lives

--- Assets/Scripts/WinLoseControl.cs
+++ Assets/Scripts/WinLoseControl.cs
@@ -6,42 +6,43 @@ using UnityEngine.UI;
 
 public class WinLoseControl : MonoBehaviour
 {
-    private int game_status = 0;
+  private int game_status = 0;
 
-    public GameObject mainScreen;
-    public GameObject winScreen;
-    public GameObject loseScreen;
+  public GameObject mainScreen;
+  public GameObject winScreen;
+  public GameObject loseScreen;
 
-    public Button restart_button;
-    public Button quit_button;
-    public Button continue_button;
-  
-    public void WinGame()
+  public Button restart_button;
+  public Button quit_button;
+  public Button continue_button;
+
+  public void WinGame()
+  {
+    if (game_status != 0)
     {
-        if (game_status != 0)
-        {
-            return;
-        }
-        game_status = 2;
-        mainScreen.SetActive(false);
-        winScreen.SetActive(true);
-        if(SceneManager.GetActiveScene().buildIndex == 1)
-        {
-            continue_button.gameObject.SetActive(true);
-        }
-		restart_button.gameObject.SetActive(true);
-		quit_button.gameObject.SetActive(true);
+      return;
     }
-    public void LoseGame()
+    game_status = 2;
+    mainScreen.SetActive(false);
+    winScreen.SetActive(true);
+    if (SceneManager.GetActiveScene().buildIndex == 1)
     {
-        if (game_status != 0)
-        {
-            return;
-        }
-        game_status = 3;
-        restart_button.gameObject.SetActive(true);
-        quit_button.gameObject.SetActive(true);
-        mainScreen.SetActive(false);
-        loseScreen.SetActive(true);
+      continue_button.gameObject.SetActive(true);
     }
+    restart_button.gameObject.SetActive(true);
+    quit_button.gameObject.SetActive(true);
+  }
+  public void LoseGame()
+  {
+    if (game_status != 0)
+    {
+      return;
+    }
+    game_status = 3;
+    restart_button.gameObject.SetActive(true);
+    quit_button.gameObject.SetActive(true);
+    continue_button.gameObject.SetActive(false);
+    mainScreen.SetActive(false);
+    loseScreen.SetActive(true);
+  }
 }
