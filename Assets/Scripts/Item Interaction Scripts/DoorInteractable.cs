d6d3a92df4f251ddf308526d534b306645961819
popdog7 <148223929+popdog7@users.noreply.github.com>
2024-11-05 10:44:07 -0600

    Revert "Revert "added Bryans room, fade ins, and door intractability.""

---
+++ Assets/Scripts/Item Interaction Scripts/DoorInteractable.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class DoorInteractable : Interactable
+{
+    private Animator door_animator;
+
+    private void Awake()
+    {
+        door_animator = GetComponent<Animator>();
+    }
+
+    protected override void Interact()
+    {
+        Debug.Log("Interacted with");
+        bool isOpen = door_animator.GetBool("isOpen");
+        if (isOpen)
+        {
+            door_animator.SetBool("isOpen", false);
+        }
+        else
+        {
+            door_animator.SetBool("isOpen", true);
+        }
+    }
+}
