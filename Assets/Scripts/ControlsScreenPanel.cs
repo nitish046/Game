9a5ba8423fcd752cfa6d3c8b0b1f87d530d31ea3
Ryan Camp <141803241+TheSimicSlaw@users.noreply.github.com>
2024-11-25 23:06:13 -0600

    Control Screen & Pause In Play

--- Assets/Scripts/ControlsScreenPanel.cs
+++ Assets/Scripts/ControlsScreenPanel.cs
@@ -2,11 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class ControlsScreenPanel : MonoBehaviour
 {
   [SerializeField] private Image controlsScreen;
-  [SerializeField] PlayerInputActions input;
+  private PlayerInputActions playerInput;
+  private InputAction viewControlsPanel;
+
+  private float enablePanel;
+  // private GameInput input;
+
+  private void Awake()
+  {
+    playerInput = new PlayerInputActions();
+  }
+
+  private void OnEnable()
+  {
+    viewControlsPanel = playerInput.Player.ViewControlsScreen;
+    viewControlsPanel.Enable();
+  }
+
+  private void OnDisable()
+  {
+    viewControlsPanel.Disable();
+  }
+
   // Start is called before the first frame update
   void Start()
   {
@@ -16,6 +38,16 @@ public class ControlsScreenPanel : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-
+    enablePanel = viewControlsPanel.ReadValue<float>();
+    if (enablePanel == 1.0f)
+    {
+      Debug.Log("Enabled");
+      controlsScreen.enabled = true;
+    }
+    else
+    {
+      Debug.Log("Disabled");
+      controlsScreen.enabled = false;
+    }
   }
 }
