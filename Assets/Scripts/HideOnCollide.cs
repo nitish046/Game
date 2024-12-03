db5b695b49595b3d9a85aeb60f63698b93f1e1b3
caleb <lazerarms@gmail.com>
2024-12-03 09:43:47 -0600

    Fixed trash can so hiding actually works
    - Changed hide on collide to change the racoons layer so it is no longer targeted

--- Assets/Scripts/HideOnCollide.cs
+++ Assets/Scripts/HideOnCollide.cs
@@ -14,10 +14,13 @@ public class HideOnCollide : MonoBehaviour
 		private bool not_entered = true;
 		private Renderer[] player_renderers;
 		private PlayerRunner movement_script;
+		private GameObject player;
 
 		
 		[SerializeField] private GameInput game_input;
 		
+		public int hiding_layer = 8;
+		private int original_layer;
 
 		private void Start()
 		{
@@ -37,6 +40,7 @@ public class HideOnCollide : MonoBehaviour
 						{
 								movement_script.enabled = false;
 						}
+						ChangeLayer(hiding_layer);
 						already_hiding = true;
 						
 				}
@@ -52,7 +56,7 @@ public class HideOnCollide : MonoBehaviour
 								movement_script.enabled = true;
 						}
 						already_hiding = false;
-
+						ChangeLayer(original_layer);
 				}
 		}
 
@@ -68,6 +72,8 @@ public class HideOnCollide : MonoBehaviour
 				{
 					player_renderers = other.gameObject.GetComponentsInChildren<Renderer>();
 					movement_script = other.gameObject.GetComponentInParent<PlayerRunner>();
+					original_layer = other.gameObject.layer;
+					player = other.gameObject;
 				}
 		}
 
@@ -80,4 +86,14 @@ public class HideOnCollide : MonoBehaviour
 				movement_script = null;
 				//Debug.Log("left collison");
 	 }
+
+    private void ChangeLayer(int layer)
+    {
+		Debug.Log("Change Layer");
+        if (player != null)
+        {
+                player.gameObject.layer = layer;
+				Debug.Log(player.gameObject.layer);   
+        }
+    }
 }
