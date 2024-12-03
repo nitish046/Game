56852afe842a2903b70f9e5b60458e4c795eed95
caleb <lazerarms@gmail.com>
2024-12-03 07:33:38 -0600

    Added a cooldown to the sprint
    - sprint now has a cooldown so you can no longer run forever

--- Assets/Scripts/CharacterScripts/PlayerScripts/StateMachines/Movement/States/GroundedStates/Moving/PlayerRunningState.cs
+++ Assets/Scripts/CharacterScripts/PlayerScripts/StateMachines/Movement/States/GroundedStates/Moving/PlayerRunningState.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 namespace MaskedMischiefNamespace
 {
-	public class PlayerRunningState : PlayerGroundedState
-	{
-		public PlayerRunningState(PlayerMovementStateMachine playerMovementStateMachine) : base(playerMovementStateMachine)
-		{
-		}
+    public class PlayerRunningState : PlayerGroundedState
+    {
+        private float sprint_time = 5f;
+        private float sprint_cooldown = 3f;
+        private float current_sprint_time;
+        private float cooldown_timer;
+        private bool is_cooldown = false;
+
+        public PlayerRunningState(PlayerMovementStateMachine playerMovementStateMachine) : base(playerMovementStateMachine)
+        {
+        }
 
         public override void Enter()
         {
@@ -21,6 +27,48 @@ namespace MaskedMischiefNamespace
             base.Exit();
             stateMachine.player.animator.ResetTrigger("isSprinting");
         }
+
+        /*
+        public override void PhysicsUpdate()
+		{
+            float speed;
+
+            Vector3 cameraDir = stateMachine.player.mainCamera.transform.rotation.eulerAngles;
+			Vector3 moveDir = Quaternion.Euler(0, cameraDir.y, 0) * new Vector3(staticMovement.x, 0, staticMovement.y);
+			//stateMachine.player.transform.Translate(moveDir * stateMachine.player.walkSpeed);
+            if (stateMachine.player.isSprinting && !is_cooldown)
+            {
+                if(current_sprint_time >= sprint_cooldown)
+                {
+                    is_cooldown = true;
+                    current_sprint_time = 0f;
+                }
+                speed = stateMachine.player.runSpeed;
+                current_sprint_time += Time.deltaTime;
+            }
+            else
+            {
+                if (cooldown_timer >= sprint_cooldown)
+                {
+                    is_cooldown = false;
+                    cooldown_timer = 0f;
+                }
+                speed = stateMachine.player.walkSpeed;
+                cooldown_timer += Time.deltaTime;
+            }
+
+			if (!moveDir.Equals(new Vector3(0, 0, 0)))
+			{
+				Quaternion moveAngle = Quaternion.LookRotation(moveDir);
+				stateMachine.player.transform.rotation = Quaternion.Slerp(stateMachine.player.transform.rotation, moveAngle, 0.2f);
+				if (!stateMachine.player.WillCollide(stateMachine.player.transform.forward, speed, new[] { "Collidable" }))
+					stateMachine.player.GetComponent<CharacterController>().Move(stateMachine.player.transform.forward * speed);
+
+			}
+			stateMachine.player.GetComponent<CharacterController>().Move(Vector3.down * -stateMachine.player.yVelocity);
+
+		}
+        */
 
     }
 }
